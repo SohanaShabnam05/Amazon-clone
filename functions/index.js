a9e729fb83278a91035097d92a8d67013666bc0c
sohana <sshabnamzsf@gmail.com>
2023-04-17 21:35:50 +0530

    Stripe added

--- functions/index.js
+++ functions/index.js
@@ -10,9 +10,30 @@ const stripe = require("stripe")
 //API
 
 // - App Config
+const app = express();
 
 // - Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
 
 // - API routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('Payment Request Recieved !!! for this amount ', total)
+
+    const paymentIntent = await stripe.paymentIntent.create({
+        amount: total, // Submit of the currency
+        currency: "rupees",
+    });
+
+    // OK - Created
+    response.status(201).send({
+        clientSecret : paymentIntent.client_secret,
+    })
+})
 
 // - Listen Command
+exports.api = functions.https.onRequest(app)
